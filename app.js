d676690a724e002d6b1d85fe8a37ae487c2c1417
MD SAHABUDDIN <32165416+mdsahabuddin007@users.noreply.github.com>
2024-01-10 21:26:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+const accessKeyId = "AKIA2TM7XGTPVPW63HXZ";
+const secretAccessKey = "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";
+const bucketName = "js3223"; 
+
+AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+    region: "ap-south-1"
+});
+
+// Create S3 instance
+const s3 = new AWS.S3();
+
+function uploadImage() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert('Please select an image file.');
+        return;
+    }
+
+    const params = {
+        Bucket: bucketName,
+        Key: file.name,
+        Body: file,
+        ACL: 'public-read' // Set the ACL to make the uploaded image publicly accessible
+    };
+
+    s3.upload(params, (err, data) => {
+        if (err) {
+            console.error(err);
+            alert('Error uploading image.');
+        } else {
+            console.log(data);
+            document.getElementById('uploadedImage').src = data.Location;
+            document.getElementById('uploadedImage').style.display = 'block';
+            alert('Image uploaded successfully.');
+        }
+    });
+}
